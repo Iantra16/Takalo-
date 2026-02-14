b58e3e227ea28f55c1ad3a6e5e054e9afc4d88a5
Moon4824 <salomonbovel@gmail.com>
2026-02-14 09:55:42 +0300

    Revert "rectification et utilisation de vue"
    This reverts commit 71350f4b62c0bb2ac5f82a66fb6295d6127bb108.

--- app/models/ObjetModel.php
+++ app/models/ObjetModel.php
@@ -13,20 +13,14 @@ class ObjetModel
         $this->db = $db;
     }
 
-    public function getAll() {
-        $sql = $this->db->query("SELECT * FROM v_objet_propri_actuel");
+    public function getAllObjets() {
+        $sql = $this->db->query("SELECT * FROM objet");
         return $sql->fetchAll();
     }
 
-    public function getMy($idUser) {
-        $sql = $this->db->prepare("SELECT * FROM v_objet_propri_actuel WHERE id_proprietaire = ?");
-        $sql->execute([$idUser]);
-        return $sql->fetchAll();
-    }
-
-    public function getOthers($idUser) {
-        $sql = $this->db->prepare("SELECT * FROM v_objet_propri_actuel WHERE id_proprietaire != ? OR id_proprietaire IS NULL");
-        $sql->execute([$idUser]);
+    public function getObjetOthers($me) {
+        $sql = $this->db->prepare("SELECT * FROM objet WHERE id_user != ?");
+        $sql->execute([$me]);
         return $sql->fetchAll();
     }
 
