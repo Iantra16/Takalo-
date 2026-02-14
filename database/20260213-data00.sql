9df25cd98130f0231749af9863b8de555c0b8919
Moon4824 <salomonbovel@gmail.com>
2026-02-14 05:01:19 +0300

    data user

---
+++ database/20260213-data00.sql
@@ -0,0 +1,8 @@
+-- ============================================
+-- TABLE USER (UTILISATEURS)
+-- ============================================
+
+INSERT INTO user (idUser, nom, prenom, email, password_hash, telephone) VALUES
+(1, 'Dupont', 'Jean', 'jean.dupont@example.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '0340000001'),
+(2, 'Curie', 'Marie', 'marie.curie@example.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '0340000002'),
+(3, 'Martin', 'Paul', 'paul.martin@example.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '0340000003');
